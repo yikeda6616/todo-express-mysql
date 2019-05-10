632d1bb54f3fa0e30622dab5aeb286c93a812dc5
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-10 14:19:03 -0700

    Add API basic scaffolding

---
+++ packages/server/entities/User.ts
@@ -0,0 +1,10 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+}
--- packages/server/server.ts
+++ packages/server/server.ts
@@ -8,8 +8,6 @@ const PORT = 3001;
 // bodyparser alternative
 app.use(express.json());
 
-const handler = express();
-
 (async () => {
   const connection = await createConnection({
     type: 'mysql',
@@ -26,8 +24,26 @@ const handler = express();
   });
 })();
 
-handler.get('/', (req, res) => {
-  res.send('hallo welt');
-});
+// Init API handler
+const handler = express();
 
+// Show user
+handler.get('/user', (req, res) => {});
+// Create user
+handler.post('/user', (req, res) => {});
+// Edit User
+handler.put('/user/:uid', (req, res) => {});
+// Delete user
+handler.delete('/user/:uid', (req, res) => {});
+
+// Show todo
+handler.get('/todo', (req, res) => {});
+// Create todo
+handler.post('/todo', (req, res) => {});
+// Edit todo
+handler.put('/todo/:tid', (req, res) => {});
+// Delete todo
+handler.delete('/todo/:tid', (req, res) => {});
+
+// Mount middleware
 app.use('/api', handler);
